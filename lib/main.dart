d0e02a003c8a5a733b3bfd251bf9586eeefd69c9
Trevor Huey <thuey100@gmail.com>
2020-07-07 19:30:46 -0500

    feat(#1) initialize project (#4)
    * feat(#1): inits flutter project
    
    * build(#1): build app
    
    * build(#1): rename
    
    * build(#1): fixed build
    
    * build(#1): fixed build
    
    * build(#1): fixed build
    
    * build(#1): fixed build
    
    * build(#1): fixed build
    
    * build(#1): fixed build
    
    * build(#1): fixed build
    
    * build(#1): include full sha
    
    * build(#1): on_master
    
    * upload entire release folder
    
    * upload entire release folder
    
    Co-authored-by: Trevor Huey <thuey@Trevors-Mac-mini.local>

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Twist',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const HomePage(title: 'Write with a Twist'),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  final String title;
+
+  const HomePage({this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text(
+              'Hello World!',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
