866f93412e978f1cdedd6ac30a81dc8105534d76
Trevor Huey <thuey100@gmail.com>
2020-07-22 23:27:32 -0500

    feat(#17): theming

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,24 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Twist',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
+      theme: ThemeData.from(
+          colorScheme: const ColorScheme(
+              primary: Color(0xff424242),
+              primaryVariant: Color(0xff1b1b1b),
+              secondary: Color(0xffab47bc),
+              secondaryVariant: Color(0xff790e8b),
+              surface: Color(0xffffffff),
+              background: Color(0xffffffff),
+              error: Color(0xbff00020),
+              onPrimary: Color(0xffffffff),
+              onSecondary: Color(0xffffffff),
+              onSurface: Color(0xff000000),
+              onBackground: Color(0xff000000),
+              onError: Color(0xffffffff),
+              brightness: Brightness.light),
+          textTheme: Theme.of(context).textTheme.merge(const TextTheme(
+              bodyText1: TextStyle(fontFamily: 'NotoSans'),
+              bodyText2: TextStyle(fontFamily: 'NotoSans')))),
       home: const HomePage(title: 'Write with a Twist'),
     );
   }
